d19c7ec79976181c17d7584340c60145152aa4ca
Malik Gray <mikey@starfantasygames.com>
2016-02-05 18:50:06 -0500

    Modified the Web App to use MVC
    Modified the Web App to use MVC instead of static HTML

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNet.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TheWorld.Controllers.Web
 {
-    public class AppController
+    public class AppController : Controller
     {
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
